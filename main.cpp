49e576d6038dade9c934bd8ece5215a54a452a43
Felipe Moleiro <felipemoleiro@hotmail.com>
2022-06-23 22:46:44 -0300

    read rom and execute it to test cpu instructions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "cpu.h"
+
 #define KB 1024
 
 void read_ines(std::string path){
@@ -22,8 +24,32 @@ void read_ines(std::string path){
     u_char PRGROM[sizePRGROM];
     fread(PRGROM,1,sizePRGROM,inesFile);
 
+    fclose(inesFile);
 }
 
+char* load_rom(std::string path){
+    FILE* rom = fopen(path.c_str(),"r");
+
+    char* memory = (char*)malloc(64*KB);
+
+    fread(memory,1,64*KB,rom);
+
+    fclose(rom);
+    return memory;
+}
+
+void printCpu(CPU &cpu){
+    std::cout << "--------------------------------------------------\n";
+    printf("PC: %.4X\n", cpu.PC);
+    printf("AC: %.2X X: %.2X Y: %.2X\n", (u_char)cpu.AC,(u_char)cpu.X,(u_char)cpu.Y);
+    printf("SR: ");
+    for(int i=7;i>=0;i--){
+        printf("%d", (cpu.SR >> i) & 1);
+    }
+    printf("\n");
+    printf("SP: %.2X\n", (u_char)cpu.SP);
+    std::cout << "--------------------------------------------------\n\n";
+}
 
 
 int main(){
@@ -49,5 +75,19 @@ int main(){
     char X = 10;
     std::cout << (u_short)(u_char)(LL + (u_char)X) << '\n'; 
     */
+    char* memory = load_rom("rom.bin");
+
+    CPU main_cpu = CPU(memory);
+
+
+    printCpu(main_cpu);
+    char stopChar;
+    scanf("%c", &stopChar);
+    while(stopChar != 's'){
+        main_cpu.nextIntruction();
+        printCpu(main_cpu);
+        scanf("%c", &stopChar);
+    }
+
     return 0;
 }
